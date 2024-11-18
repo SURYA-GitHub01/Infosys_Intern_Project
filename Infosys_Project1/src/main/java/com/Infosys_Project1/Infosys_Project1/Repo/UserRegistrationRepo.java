5415561d71f8f447cc46c373cbb652e3c5705397
SURYA S <140172921+SURYA-GitHub01@users.noreply.github.com>
2024-11-18 11:26:05 +0530

    Delete Infosys_Project1 directory

--- Infosys_Project1/src/main/java/com/Infosys_Project1/Infosys_Project1/Repo/UserRegistrationRepo.java
+++
@@ -1,9 +0,0 @@
-package com.Infosys_Project1.Infosys_Project1.Repo;
-
-import com.Infosys_Project1.Infosys_Project1.Entity.UserRegistrationDTO;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRegistrationRepo extends JpaRepository<UserRegistrationDTO,Long> {
-}
